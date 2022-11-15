364b3f0b7e038f65d6f744adb4fd5e0fb4f03711
petro_thomas <peter88tom@gmail.com>
2022-11-16 00:05:23 +0200

    Add pets API  call to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import Header from './components/Header';
 import {Routes, Route} from '@solidjs/router'
 import Home from './pages/Home';
+import LikedPets from './pages/LikedPets';
+
+// Pet api call
+import { createEffect, createSignal } from "solid-js";
+
+const [pets, setPets] = createSignal([]);
+
+createEffect(async () => {
+  const res = await fetch(`http://pets-v2.dev-apis.com/pets`)
+  const json = await res.json()
+  setPets(json.pets)
+  console.log(json.pets);
+});
 
 function App() {
   return (
