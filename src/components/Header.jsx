fd07dec2d3bc902fb1b5c8e1b3a162a3b8906d23
petro_thomas <peter88tom@gmail.com>
2022-11-16 00:28:31 +0200

    Import pets state from the App component and loop through

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 // Header component
+import { For } from "solid-js";
+import { pets } from "../App";
+import PetCard from "./PetCard";
 
 const Header = () =>{
 	return (
 		<div>
-			<h2>Header Component</h2>
+			<h2>List of Awesome Pets</h2>
+			<For each={ pets() }>
+				{(pet => <PetCard pet={pet} /> )}
+			</For>
 		</div>
 	)
 	
 }
 
 export default Header;
+
+
+/*
+ 1. We imported the pets state from the App component and loop throug the array with For template helper and render
+    the PetCard component with each item from the pets array.
+
+    The PetCard components takes takes a prop of item as pet
+*/
