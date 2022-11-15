4e4fd03314b4095ba0f4cb00cef7554dae8222d8
petro_thomas <peter88tom@gmail.com>
2022-11-15 23:09:04 +0200

    Install router and boostrap library

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,20 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
+import  App  from "./App"
 
-import './index.css';
-import App from './App';
+import { Router } from "@solidjs/router";
+import "bootstrap/dist/css/bootstrap.min.css"
 
-render(() => <App />, document.getElementById('root'));
+render(
+	() => (
+	<Router>
+		<App />
+	</Router>
+	 ),
+	document.getElementById('root')
+     );
+/*
+
+1. We imported the Route compontens from solid-app-route and wrap it around the App component, then we imported the bootstrap 
+   CSS  library.
+*/
