e6bcdc060d01683343633d8fc7f6ddbda85f6c4a
petro_thomas <peter88tom@gmail.com>
2022-11-15 23:17:15 +0200

    Create Header and PetCard components with placeholders

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// Header component
+
+const Header = () =>{
+	return (
+		<div>
+			<h2>Header Component</h2>
+		</div>
+	)
+	
+}
+
+export default Header;
---
+++ src/components/PetCard.jsx
@@ -0,0 +1,11 @@
+// PetCard component
+
+const PetCard = () => {
+	return (
+		<div>
+			<h2>PetCard Component</h2>
+		</div>
+	);
+}
+
+export default PetCard;
