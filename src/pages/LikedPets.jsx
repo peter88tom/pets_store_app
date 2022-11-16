2a33be9c439389bf4c8b5bb8d9943adf6dc176ee
petro_thomas <peter88tom@gmail.com>
2022-11-16 06:02:48 +0200

    Update LikedPets create array of pets

--- src/pages/LikedPets.jsx
+++ src/pages/LikedPets.jsx
@@ -1,11 +1,25 @@
 // LikedPets component
+import { createSignal, For } from 'solid-js';
+import PetCard from '../components/PetCard';
+
+
+const  [cutePets, setCutePets] = createSignal([]);
 
 const LikedPets = () => {
 	return (
 		<div>
-			<h2>LikedPets Components</h2>
+			<For each={cutePets()}>
+				{pet => <PetCard pet={pet} />}
+			</For>
 		</div>
 	);
 }
-
+export {cutePets, setCutePets}
 export default LikedPets;
+
+/*
+1. We are using Signal to create an Array of cute pets. Then we loop through
+ the array of cutePets and render the PetCard component for each item in the array.
+
+2. We export the functions to use it in other components.
+*/
